69d7186d4b6d8b3f7b470f813c12366d8049b6fd
ttoonnaa <yunocchi@gmail.com>
2024-12-06 21:19:48 +0900

    レベルアップちまちま…

--- js/plugins/_Tona_Actor.js
+++ js/plugins/_Tona_Actor.js
@@ -1,25 +1,25 @@
 
-// **************************************************************
+// ****************************************************************************************************************************
 // Game_Actor
-// --------------------------------------------------------------
+// ----------------------------------------------------------------------------------------------------------------------------
 
 (function() {
 
-	// **************************************************************
+	// ****************************************************************************************************************************
 	// 初期化
-	// --------------------------------------------------------------
+	// ----------------------------------------------------------------------------------------------------------------------------
 
 	var Game_Actor_initMembers = Game_Actor.prototype.initMembers;
 	Game_Actor.prototype.initMembers = function() {
 		Game_Actor_initMembers.call(this);
 
-		this._personality = 1;
+		this._personalityId = 1;
 		this.clearParamBase();
 	};
 
-	// **************************************************************
+	// ****************************************************************************************************************************
 	// セットアップ
-	// --------------------------------------------------------------
+	// ----------------------------------------------------------------------------------------------------------------------------
 
 	var Game_Actor_setup = Game_Actor.prototype.setup;
 	Game_Actor.prototype.setup = function(actorId) {
@@ -27,27 +27,29 @@
 
 		this.setupParamBase();
 	    this.recoverAll();
+
+		console.log(this);
 	};
 
-	// **************************************************************
+	// ****************************************************************************************************************************
 	// 性格
-	// --------------------------------------------------------------
+	// ----------------------------------------------------------------------------------------------------------------------------
 
 	Game_Actor.prototype.personality = function() {
-	    return this._personality;
+		return $_tona_Personality[this._personalityId];
 	};
 
 	Game_Actor.prototype.personalityName = function() {
-		return $_tona_Personality[this._personality].name;
+		return $_tona_Personality[this._personalityId].name;
 	};
 
-	Game_Actor.prototype.setPersonality = function(personality) {
-	    this._personality = personality;
+	Game_Actor.prototype.setPersonalityId = function(personalityId) {
+	    this._personalityId = _personalityId;
 	};
 
-	// **************************************************************
+	// ****************************************************************************************************************************
 	// パラメーター
-	// --------------------------------------------------------------
+	// ----------------------------------------------------------------------------------------------------------------------------
 
 	Game_Actor.prototype.paramBase = function(paramId) {
 	    return this._paramBase[paramId];
@@ -59,29 +61,49 @@
 	}
 
 	Game_Actor.prototype.setupParamBase = function() {
-    	const actor = $dataActors[this._actorId];
+		var klass = this.currentClass();
 
 		// レベル１の値で初期化する
-		this._paramBase = [10, 10, 10, 10, 10, 10, 10, 10];
+		for (var i = 0; i < 8; i++) {
+			this._paramBase[i] = klass.params[i][1];
+		}
 	}
 
-	// **************************************************************
+	// ****************************************************************************************************************************
 	// レベルアップ
-	// --------------------------------------------------------------
+	// ----------------------------------------------------------------------------------------------------------------------------
 
 	var Game_Actor_levelUp = Game_Actor.prototype.levelUp;
 	Game_Actor.prototype.levelUp = function() {
 		Game_Actor_levelUp.call(this);
 
-		// 性格を考慮して paramBase を与える
+		var klass = this.currentClass();
+		var personality = this.personality();
+
+		// 性格を考慮して paramBase を計算
 		for (var i = 0; i < 8; i++) {
-			this._paramBase[i] += 0;
+			var paramBase = this._paramBase[i];
+			var paramBaseAmari = this._paramBaseAmari[i];
+			var paramDiff = klass.params[i][this._level] - klass.params[i][this._level - 1];
+
+			paramBase += Math.floor(paramDiff * (100 + personality.params[i]) / 100);
+			paramBaseAmari += paramDiff * (100 + personality.params[i]) % 100;
+
+			while (paramBaseAmari >= 100) {
+				paramBase ++;
+				paramBaseAmari -= 100;
+			}
+
+			this._paramBase[i] += paramBase;
+			this._paramBaseAmari[i] = paramBaseAmari;
 		}
+
+	    this.refresh();
 	};
 
-	// **************************************************************
+	// ****************************************************************************************************************************
 	// 転職
-	// --------------------------------------------------------------
+	// ----------------------------------------------------------------------------------------------------------------------------
 
 	var Game_Actor_changeClass = Game_Actor.prototype.changeClass;
 	Game_Actor.prototype.changeClass = function(classId, keepExp) {
@@ -90,6 +112,7 @@
 		// paramBase を半分にする（切り上げ）
 		for (var i = 0; i < 8; i++) {
 			this._paramBase[i] = Math.ceil(this._paramBase[i] / 2);
+			this._paramBaseAmari[i] = 0;
 		}
 
 	    this.refresh();
--- js/plugins/_Tona_Main.js
+++ js/plugins/_Tona_Main.js
@@ -1,12 +1,42 @@
 
-// **************************************************************
+// ****************************************************************************************************************************
 // Tona_Main
-// --------------------------------------------------------------
+// ----------------------------------------------------------------------------------------------------------------------------
 
 (function() {
 
-	// 性格を作成
-	_tona_CreatePersonalityData();
+	// データベースを作成
+	_tona_CreateDatabase();
 
 })();
 
+// ****************************************************************************************************************************
+// データベース読み込み後の編集
+// ----------------------------------------------------------------------------------------------------------------------------
+
+(function() {
+
+	var DataManager_onLoad = DataManager.onLoad;
+	DataManager.onLoad = function(object) {
+		DataManager_onLoad.call(this, object);
+		_tona_modifyDatabase(object);
+	}
+})();
+
+function _tona_modifyDatabase(object) {
+
+    if (false) {}
+
+	// アクター
+	//else if (object === $dataActors) {
+    //    _tona_Data_OverrideActorDatabase();
+    //}
+	// エネミー
+	//else if (object === $dataEnemies) {
+    //    _tona_Data_OverrideEnemyDatabase();
+    //}
+    // クラス
+	else if (object === $dataClasses) {
+        _tona_Data_OverrideClassDatabase();
+    }
+}
