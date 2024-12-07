72c6dac7b5ce7dc606875eb6434089e1f4f1c6aa
ttoonnaa <yunocchi@gmail.com>
2024-12-07 23:34:38 +0900

    転職の計算式を実装

--- js/plugins/_Tona_Actor.js
+++ js/plugins/_Tona_Actor.js
@@ -27,8 +27,6 @@
 
 		this.setupParamBase();
 	    this.recoverAll();
-
-		console.log(this);
 	};
 
 	// ****************************************************************************************************************************
@@ -64,9 +62,12 @@
 		var klass = this.currentClass();
 
 		// レベル１の値で初期化する
-		for (var i = 0; i < 8; i++) {
+		for (var i = 2; i < 8; i++) {
 			this._paramBase[i] = klass.params[i][1];
 		}
+
+		// パラメーターを補正
+		this._tona_refreshParam();
 	}
 
 	// ****************************************************************************************************************************
@@ -77,29 +78,47 @@
 	Game_Actor.prototype.levelUp = function() {
 		Game_Actor_levelUp.call(this);
 
-		var klass = this.currentClass();
-		var personality = this.personality();
+		// レベル 0 -> 1 は成長しない
+		if (this.level >= 2) {
 
-		// 性格を考慮して paramBase を計算
-		for (var i = 0; i < 8; i++) {
-			var paramBase = this._paramBase[i];
-			var paramBaseAmari = this._paramBaseAmari[i];
-			var paramDiff = klass.params[i][this._level] - klass.params[i][this._level - 1];
+			var klass = this.currentClass();
+			var personality = this.personality();
 
-			paramBase += Math.floor(paramDiff * (100 + personality.params[i]) / 100);
-			paramBaseAmari += paramDiff * (100 + personality.params[i]) % 100;
-
-			while (paramBaseAmari >= 100) {
-				paramBase ++;
-				paramBaseAmari -= 100;
+			// 性格を考慮して paramBase を計算
+			for (var i = 2; i < 8; i++) {
+				var paramDiff = klass.params[i][this._level] - klass.params[i][this._level - 1];
+				this._paramBase[i] += Math.floor(paramDiff * (100 + personality.params[i]) / 100);
+				this._paramBaseAmari[i] += paramDiff * (100 + personality.params[i]) % 100;
 			}
-
-			this._paramBase[i] += paramBase;
-			this._paramBaseAmari[i] = paramBaseAmari;
 		}
 
+		// パラメーターを補正
+		this._tona_refreshParam();
 	    this.refresh();
 	};
+
+	// ****************************************************************************************************************************
+	// パラメーターを補正
+	// ----------------------------------------------------------------------------------------------------------------------------
+
+	Game_Actor.prototype._tona_refreshParam = function() {
+
+		// HPを計算（体力の2倍）（種の分も含む）
+		this._paramBase[0] = (this._paramBase[5] + this._paramPlus[5]) * 2;
+		this._paramBaseAmari[0] = this._paramBaseAmari[5] * 2;
+
+		// MPを計算（賢さの2倍）（種の分も含む）
+		this._paramBase[1] = (this._paramBase[4] + this._paramPlus[4]) * 2;
+		this._paramBaseAmari[1] = this._paramBaseAmari[4] * 2;
+
+		// 余りを繰り越し
+		for (var i = 0; i < 8; i++) {
+			while (this._paramBaseAmari[i] >= 100) {
+				this._paramBase[i] ++;
+				this._paramBaseAmari[i] -= 100;
+			}
+		}
+	}
 
 	// ****************************************************************************************************************************
 	// 転職
@@ -107,6 +126,10 @@
 
 	var Game_Actor_changeClass = Game_Actor.prototype.changeClass;
 	Game_Actor.prototype.changeClass = function(classId, keepExp) {
+
+		// 新しいクラスの経験値を 0 にしてから元の処理を呼ぶ
+		this._exp[classId] = 0;
+
 		Game_Actor_changeClass.call(this, classId, keepExp);
 
 		// paramBase を半分にする（切り上げ）
